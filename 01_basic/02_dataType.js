a0424839205d6d2af11b50a8023bd15083cc5314
vivekkasaudhan <vivekkasaudhan241@gmail.com>
2025-05-28 09:42:22 +0000

    datatypes

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,11 @@
+"use strict"// treat all js code as newer version
+alert(3+3)// we are using node js not browser
+console.log(3+3)
+// number=> 2 to power 53
+// bigint
+//string
+// boolean
+//null=>standlone value
+//symbol
+// object 
+console.log(typeof(a))// to find type of variable
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,8 @@
-console.log("Vivek")
+
+let a=30;
+let name="vivek"
+let age;
+console.log(name,a)
+console.table([name,a,age])
+
+
